e1d4e568aed0a7376bc0dfa28533d334cb6066e0
SamirManafDev <samirmanafli161@gmail.com>
2023-03-03 03:12:51 -0800

    Task 3 March

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿//Task 3 March
+
+//int[] numbers = { 1, 2, 3, 5, 10, 15, 27 };
+//for (int i = numbers.Length-1; i >=0; i--)
+//{
+//    Console.WriteLine(numbers[i]);
+//}
+
+
+
+
+
+
+//int[] numbers = { 1, 2, 3, 5, 10, 15, 27 };
+//int sum = 0;
+//for(int i = 0; i < numbers.Length; i++)
+//{
+//    if (numbers[i] <= 9)
+//    {
+//        sum++;
+//    } 
+//}
+//Console.WriteLine(sum);
+
+
+
+
